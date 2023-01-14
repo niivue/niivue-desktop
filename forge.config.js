0a776d47fe4e746e27648dad0b2c927f0bf27604
Taylor Hanayik <hanayik@gmail.com>
2023-01-14 10:17:20 +0000

    add example apple signing and notarizing config

--- forge.config.js
+++ forge.config.js
@@ -1,5 +1,15 @@
 module.exports = {
-  packagerConfig: {},
+  packagerConfig: {
+    ignore: "(.git|.vscode|niivue|node_modules|.gitignore|README.md|LICENSE.md)",
+    icon: "./niivue",
+    osxSign: {},
+    osxNotarize: {
+      tool: 'notarytool',
+      appleId: process.env.APPLE_ID,
+      appleIdPassword: process.env.APPLE_PASSWORD,
+      teamId: process.env.APPLE_TEAM_ID
+    },
+  },
   rebuildConfig: {},
   makers: [
     {
